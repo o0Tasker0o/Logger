a646a26ef8a15fcacc468dddea781b209d88eb5e
James Tasker <james_tasker1@hotmail.com>
2015-09-28 14:14:08 +0100

    Create log entry object
    Signed-off-by: James Tasker <james_tasker1@hotmail.com>

--- LoggerLib/LogEntry.cs
+++ LoggerLib/LogEntry.cs
@@ -1,6 +1,34 @@
-﻿namespace LoggerLib
+﻿using System;
+namespace LoggerLib
 {
     public class LogEntry
     {
+        public String Text
+        {
+            get;
+            private set;
+        }
+
+        public DateTime CreatedTime
+        {
+            get;
+            private set;
+        }
+
+        public LogEntry(string text)
+        {
+            if(null == text)
+            {
+                throw new ArgumentNullException("Log text must not be null");
+            }
+
+            if(String.IsNullOrEmpty(text))
+            {
+                throw new ArgumentException("Log text must not be empty");
+            }
+
+            Text = text;
+            CreatedTime = DateTime.Now;
+        }
     }
 }
---
+++ LoggerLibTests/LogEntryTests.cs
@@ -0,0 +1,45 @@
+﻿using LoggerLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace LoggerLibTests
+{
+    [TestClass]
+    public class LogEntryTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void LogEntryThrowsExceptionWhenGivenNullText()
+        {
+            LogEntry entry = new LogEntry(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void LogEntryThrowsExceptionWhenGivenEmptyText()
+        {
+            LogEntry entry = new LogEntry("");
+        }
+
+        [TestMethod]
+        public void LogEntryStoresCurrentTimeWhenConstructed()
+        {
+            LogEntry entry = new LogEntry("test");
+
+            DateTime currentTime = DateTime.Now;
+            double delta = 2.0 * TimeSpan.TicksPerSecond;
+
+            Assert.AreEqual(currentTime.Ticks, entry.CreatedTime.Ticks, delta);
+        }
+
+        [TestMethod]
+        public void LogEntryStoresInputText()
+        {
+            const string cEntryText = "This is a log entry";
+
+            LogEntry entry = new LogEntry(cEntryText);
+
+            Assert.AreEqual(cEntryText, entry.Text);
+        }
+    }
+}
