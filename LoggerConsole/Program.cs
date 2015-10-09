17b03c354a8ccd6a7b3906a87654f03d1fa3fc8e
James Tasker <james_tasker1@hotmail.com>
2015-10-09 16:11:05 +0100

    Add basic console app to store log entries
    Signed-off-by: James Tasker <james_tasker1@hotmail.com>

---
+++ LoggerConsole/Program.cs
@@ -0,0 +1,35 @@
+﻿using LoggerLib;
+using System;
+using System.Linq;
+
+namespace LoggerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (LoggerDbContext loggerDatabase = new LoggerDbContext())
+            {
+                Console.Write("Enter a log entry: ");
+                string entryText = Console.ReadLine();
+
+                LogEntry entry = new LogEntry(entryText);
+                loggerDatabase.LogEntries.Add(entry);
+                loggerDatabase.SaveChanges();
+
+                var logEntries = from logEntry in loggerDatabase.LogEntries
+                                 orderby logEntry.Text
+                                 select logEntry;
+
+                Console.WriteLine("All entries in the database:");
+                foreach (var item in logEntries)
+                {
+                    Console.WriteLine(item.Text);
+                }
+
+                Console.WriteLine("Press enter to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
