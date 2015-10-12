6667d8bdcf4814b884c986e05a2152c648a2d20b
James Tasker <james_tasker1@hotmail.com>
2015-10-12 13:16:12 +0100

    Add more sophisticated log entry and searching operations
    Signed-off-by: James Tasker <james_tasker1@hotmail.com>

--- LoggerConsole/Program.cs
+++ LoggerConsole/Program.cs
@@ -8,28 +8,67 @@ namespace LoggerConsole
     {
         static void Main(string[] args)
         {
-            using (LoggerDbContext loggerDatabase = new LoggerDbContext())
+            Console.WriteLine("Daily Log");
+            Console.WriteLine("Type to make an entry. Hit Enter for options");
+
+            while(true)
             {
-                Console.Write("Enter a log entry: ");
+                Console.Write("> ");
                 string entryText = Console.ReadLine();
 
+                if(String.IsNullOrEmpty(entryText))
+                {
+                    DisplayLogEntries();
+                }
+                else
+                {
+                    StoreLogEntry(entryText);
+                }
+            }
+        }
+
+        private static void StoreLogEntry(String entryText)
+        {
+            using (LoggerDbContext loggerDatabase = new LoggerDbContext())
+            {
                 LogEntry entry = new LogEntry(entryText);
                 loggerDatabase.LogEntries.Add(entry);
                 loggerDatabase.SaveChanges();
+            }
+        }
+
+        private static void DisplayLogEntries()
+        {
+            using (LoggerDbContext loggerDatabase = new LoggerDbContext())
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter the term you wish to search for:");
+
+                String searchTerm = Console.ReadLine();
+
+                Console.WriteLine("Please enter the date to start searching from:");
+
+                DateTime startDate = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter the date to search up to:");
+
+                DateTime endDate = DateTime.Parse(Console.ReadLine());
 
                 var logEntries = from logEntry in loggerDatabase.LogEntries
-                                 orderby logEntry.Text
+                                 where logEntry.Text.Contains(searchTerm) &&
+                                 logEntry.CreatedTime > startDate &&
+                                 logEntry.CreatedTime < endDate
+                                 orderby logEntry.CreatedTime
                                  select logEntry;
 
-                Console.WriteLine("All entries in the database:");
-                foreach (var item in logEntries)
+                foreach (var logEntry in logEntries)
                 {
-                    Console.WriteLine(item.Text);
+                    Console.WriteLine(logEntry.CreatedTime + "> " + logEntry.Text);
                 }
-
-                Console.WriteLine("Press enter to exit...");
-                Console.ReadKey();
             }
+
+            Console.WriteLine();
+            Console.WriteLine("End of logs. Type to make another entry");
         }
     }
 }
