d90f2aea04d1ee3e34deae34760c7b198eeee197
James Tasker <james_tasker1@hotmail.com>
2015-10-14 09:02:17 +0100

    Refactor date parsing code
    Signed-off-by: James Tasker <james_tasker1@hotmail.com>

--- LoggerConsole/CommandRunner.cs
+++ LoggerConsole/CommandRunner.cs
@@ -63,29 +63,13 @@ namespace LoggerConsole
         private void DisplayLogEntries()
         {
             mConsole.SetColour(ConsoleColor.DarkCyan);
-            mConsole.WriteLine("Please enter the term you wish to search for:");
+            mConsole.Write("Please enter the term you wish to search for: ");
 
             mConsole.SetColour(ConsoleColor.Gray);
             String searchTerm = mConsole.ReadLine();
 
-            mConsole.SetColour(ConsoleColor.Gray);
-
-            DateTime startDate;
-            DateTime endDate;
-
-            do
-            {
-                mConsole.SetColour(ConsoleColor.DarkCyan);
-                mConsole.WriteLine("Please enter the date to start searching from:");
-                mConsole.SetColour(ConsoleColor.Gray);
-            } while (!DateTime.TryParse(mConsole.ReadLine(), out startDate));
-
-            do
-            {
-                mConsole.SetColour(ConsoleColor.DarkCyan);
-                mConsole.WriteLine("Please enter the date to search up to:");
-                mConsole.SetColour(ConsoleColor.Gray);
-            } while (!DateTime.TryParse(mConsole.ReadLine(), out endDate));
+            DateTime startDate = GetDate("Please enter the date to start searching from: ");
+            DateTime endDate = GetDate("Please enter the date to search up to: ");
 
             var logEntries = from logEntry in mLog.GetEntries()
                                 where logEntry.Text.Contains(searchTerm) &&
@@ -103,6 +87,20 @@ namespace LoggerConsole
             mConsole.WriteLine("");
             mConsole.WriteLine("End of logs. Type to make another entry");
             mConsole.SetColour(ConsoleColor.Gray);
+        }
+
+        private DateTime GetDate(string instruction)
+        {
+            DateTime parsedDate;
+
+            do
+            {
+                mConsole.SetColour(ConsoleColor.DarkCyan);
+                mConsole.Write(instruction);
+                mConsole.SetColour(ConsoleColor.Gray);
+            } while (!DateTime.TryParse(mConsole.ReadLine(), out parsedDate));
+
+            return parsedDate;
         }
     }
 }
