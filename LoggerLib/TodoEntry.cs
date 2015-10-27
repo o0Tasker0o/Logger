0452687add793dba6d834cebb732a32bafa6248f
James Tasker <james_tasker1@hotmail.com>
2015-10-27 12:05:08 +0000

    Add TodoEntry class
    Signed-off-by: James Tasker <james_tasker1@hotmail.com>

---
+++ LoggerLib/TodoEntry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LoggerLib
+{
+    public class TodoEntry
+    {
+        public String Text
+        {
+            get;
+            private set;
+        }
+
+        public TodoEntry(String text)
+        {
+            if (null == text)
+            {
+                throw new ArgumentNullException("TODO text must not be null");
+            }
+
+            if (String.IsNullOrEmpty(text))
+            {
+                throw new ArgumentException("TODO text must not be empty");
+            }
+
+            Text = text;
+        }
+    }
+}
---
+++ LoggerLibTests/TodoEntryTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LoggerLib;
+
+namespace LoggerLibTests
+{
+    [TestClass]
+    public class TodoEntryTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TodoEntryThrowsExceptionWhenGivenNullText()
+        {
+            TodoEntry entry = new TodoEntry(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TodoEntryThrowsExceptionWhenGivenEmptyText()
+        {
+            TodoEntry entry = new TodoEntry("");
+        }
+
+        [TestMethod]
+        public void TodoEntryStoresInputText()
+        {
+            const string cEntryText = "This is a log entry";
+
+            TodoEntry entry = new TodoEntry(cEntryText);
+
+            Assert.AreEqual(cEntryText, entry.Text);
+        }
+    }
+}
